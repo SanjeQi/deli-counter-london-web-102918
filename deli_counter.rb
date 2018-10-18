20ee21e4526ff15545bc720b46a738d1f55cdc18
bright-reducer-1915 <cristi.nord@gmail.com>
2018-10-18 15:48:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,11 @@
 def line(arr)
+  if arr.lenght >= 1 
+    new_arr = []
+    counter = 1
+    arr.each do |name|
+      new_arr.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts ""
+    
   
